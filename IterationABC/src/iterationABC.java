6ee5181048aebb1b100c6a0714dc33ea771dadd3
Anant Kaul <56610952+anantkaul@users.noreply.github.com>
2019-10-18 18:36:04 +0530

    Add files via upload

---
+++ IterationABC/src/iterationABC.java
@@ -0,0 +1,15 @@
+
+public class iterationABC 
+{
+	public static void main(String args[])
+	{
+		for(char i = 'A'; i<='E'; i++)
+		{
+			for(char j = 'A'; j<=i; j++)
+			{
+				System.out.print(j+" ");
+			}
+			System.out.println();
+		}
+	}
+}
